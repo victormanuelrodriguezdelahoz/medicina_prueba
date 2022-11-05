a74874424f47ca2efabcdb3e9553699257762b36
victormanuelrodriguezdelahoz <vicmanuel2308@gmail.com>
2022-11-04 23:59:57 -0500

    'mensaje'

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from pydantic import BaseModel
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 from joblib import load
-import pickle
 
 app = FastAPI()
 
@@ -18,6 +17,10 @@ app = FastAPI()
 tablas_enc = load('tables.joblib')
 clf1 = load('clf1.joblib')
 clf2 = load('clf2.joblib')
+tablas_enc2 = load('tables2.joblib')
+clf11 = load('clf11.joblib')
+clf22 = load('clf22.joblib')
+
 
 class Item(BaseModel):
   Edadcat: str
@@ -37,44 +40,99 @@ class Item(BaseModel):
   #cuarto_diagnosticocat: str
   T_a1cat:str
   T_a2cat: str
-
-
+  perimetro: str
+  condicion: str
+  sp02: str
 
 @app.post("/")
 async def root(item: Item):
-    dato_en=[]
-    dato_en.append(int(item.Edadcat))
-    dato_en.append(tablas_enc['sexo'].transform([[item.sexocat]])[0][0])
-    dato_en.append(float(item.Tallacat))
-    dato_en.append(tablas_enc['grupo_sanguineo'].fit_transform([[item.grupo_sanguineocat]])[0][0])
-    dato_en.append(float(item.Pesocat))
-    dato_en.append(float(item.Tempcat))
-    dato_en.append(int(item.F_Ccat))#F_C
-    dato_en.append(int(item.F_Rcat))#F_R
-    dato_en.append(float(item.TEST_DE_FINDRISKcat))#Test de frindrisk
-    dato_en.append(float(item.TASA_DE_FILTRACION_GLOMERULARcat))#Tasa de filtracion
-    dato_en.append(float(item.TEST_DE_FRAMINGHAMcat))#Test de framingan ,porcentaje
-    dato_en.append(tablas_enc['diagnostico_principal'].fit_transform([[item.diagnostico_principalcat]])[0][0])#diagnostico_principal
-    #dato_en.append(tablas_enc['diagnostico_secundario'].fit_transform([[item.diagnostico_secundariocat]]))#diagnostico_secundario
-    #dato_en.append(tablas_enc['diagnostico_terciario'].fit_transform([[item.diagnostico_terciariocat]]))#diagnostico_terciario
-    #dato_en.append(tablas_enc['cuarto_diagnostico'].fit_transform([[item.cuarto_diagnosticocat]]))#cuarto_diagnostico
-    dato_en.append(int(item.T_a1cat))#T_a1
-    dato_en.append(int(item.T_a2cat))#T_a2
-    prediccion= clf1.predict([dato_en])
-    lists = prediccion.tolist()
-    prediccion2= clf2.predict(lists)
-    lists2 = prediccion2.tolist()
-    respuesta3=0
-    respuesta3={
-        "cups_solictud":tablas_enc['cups_solictud'].inverse_transform([[prediccion[0][0]]])[0][0],
-        "descripcion" :tablas_enc['descripcion'].inverse_transform([[prediccion[0][1]]])[0][0],
-        "cantidad":tablas_enc['cantidad'].inverse_transform([[prediccion[0][2]]])[0][0],
-        "prescripcion": tablas_enc['prescripcion'].inverse_transform([[prediccion[0][3]]])[0][0][0][0],
-        "tipo": tablas_enc['tipo'].inverse_transform([[prediccion[0][4]]])[0][0],
-        "codigo_medicamento":tablas_enc['codigo_medicamento'].inverse_transform([[prediccion2[0][0]]])[0][0],
-        "descripcion_medicamento":tablas_enc['descripcion_medicamento'].inverse_transform([[prediccion2[0][1]]])[0][0],
-        "cantidad_medicamento":tablas_enc['cantidad_medicamento'].inverse_transform([[prediccion2[0][2]]])[0][0],
-        "prescripcion_medicamento":tablas_enc['prescripcion_medicamento'].inverse_transform([[prediccion2[0][3]]])[0][0],
-        #"presentacion":tablas_enc['presentacion'].inverse_transform([[prediccion2[0][4]]])[0][0]
-        }
+    if item.condicion=='2':
+        dato_en=[]
+        dato_en.append(int(item.Edadcat))
+        dato_en.append(tablas_enc['sexo'].transform([[item.sexocat]])[0][0])
+        dato_en.append(float(item.Tallacat))
+        dato_en.append(tablas_enc['grupo_sanguineo'].fit_transform([[item.grupo_sanguineocat]])[0][0])
+        dato_en.append(float(item.Pesocat))
+        dato_en.append(float(item.Tempcat))
+        dato_en.append(int(item.F_Ccat))#F_C
+        dato_en.append(int(item.F_Rcat))#F_R
+        dato_en.append(float(item.TEST_DE_FINDRISKcat))#Test de frindrisk
+        dato_en.append(float(item.TASA_DE_FILTRACION_GLOMERULARcat))#Tasa de filtracion
+        dato_en.append(float(item.TEST_DE_FRAMINGHAMcat))#Test de framingan ,porcentaje
+        dato_en.append(tablas_enc['diagnostico_principal'].fit_transform([[item.diagnostico_principalcat]])[0][0])#diagnostico_principal
+        #dato_en.append(tablas_enc['diagnostico_secundario'].fit_transform([[item.diagnostico_secundariocat]]))#diagnostico_secundario
+        #dato_en.append(tablas_enc['diagnostico_terciario'].fit_transform([[item.diagnostico_terciariocat]]))#diagnostico_terciario
+        #dato_en.append(tablas_enc['cuarto_diagnostico'].fit_transform([[item.cuarto_diagnosticocat]]))#cuarto_diagnostico
+        dato_en.append(int(item.T_a1cat))#T_a1
+        dato_en.append(int(item.T_a2cat))#T_a2
+        prediccion= clf1.predict([dato_en])
+        lists = prediccion.tolist()
+        prediccion2= clf2.predict(lists)
+        lists2 = prediccion2.tolist()
+        respuesta3=0
+        respuesta3={
+            "cups_solictud":tablas_enc['cups_solictud'].inverse_transform([[prediccion[0][0]]])[0][0],
+            "cups_solicitud_probabilidad":max(clf1.predict_proba([dato_en])[0][0]),
+            "descripcion" :tablas_enc['descripcion'].inverse_transform([[prediccion[0][1]]])[0][0],
+            "descripcion_probabilidad":max(clf1.predict_proba([dato_en])[1][0]),
+            "cantidad":tablas_enc['cantidad'].inverse_transform([[prediccion[0][2]]])[0][0],
+            "cantidad_probabilidad":max(clf1.predict_proba([dato_en])[2][0]),
+            "prescripcion": tablas_enc['prescripcion'].inverse_transform([[prediccion[0][3]]])[0][0][0][0],
+            "prescripcion_probabilidad":max(clf1.predict_proba([dato_en])[3][0]),
+            "tipo": tablas_enc['tipo'].inverse_transform([[prediccion[0][4]]])[0][0],
+            "tipo_probabilidad":max(clf1.predict_proba([dato_en])[4][0]),
+            "codigo_medicamento":tablas_enc['codigo_medicamento'].inverse_transform([[prediccion2[0][0]]])[0][0],
+            "codigo_medicamento_probabilidad":max(clf2.predict_proba(lists)[0][0]),
+            "descripcion_medicamento":tablas_enc['descripcion_medicamento'].inverse_transform([[prediccion2[0][1]]])[0][0],
+            "descripcion_medicamento_probabilidad":max(clf2.predict_proba(lists)[1][0]),
+            "cantidad_medicamento":tablas_enc['cantidad_medicamento'].inverse_transform([[prediccion2[0][2]]])[0][0],
+            "cantidad_medicamento_probabilidad":max(clf2.predict_proba(lists)[2][0]),
+            "prescripcion_medicamento":tablas_enc['prescripcion_medicamento'].inverse_transform([[prediccion2[0][3]]])[0][0],
+            "prescripcion_medicamento_probabilidad":max(clf2.predict_proba(lists)[3][0]),
+            #"presentacion":tablas_enc['presentacion'].inverse_transform([[prediccion2[0][4]]])[0][0]
+            }
+    elif item.condicion=='1':
+        dato_en=[]
+        dato_en.append(int(item.Edadcat)) #Edad
+        dato_en.append(tablas_enc2['sexo'].transform([[item.sexocat]])[0][0]) #sexo
+        dato_en.append(tablas_enc2['grupo_sanguineo'].fit_transform([[item.grupo_sanguineocat]])[0][0])#grupo_sanguineo
+        dato_en.append(float(item.Pesocat)) #peso 
+        dato_en.append(float(item.Tallacat)) #talla
+        dato_en.append(float(item.Tempcat)) #temperatura
+        dato_en.append(int(item.F_Ccat))#F_C 
+        dato_en.append(float(item.perimetro))#perimetro cefalico
+        dato_en.append(float(item.F_Rcat))#frecuencia respiratoria
+        dato_en.append(float(item.sp02))#spO2
+        dato_en.append(tablas_enc2['diagnostico_principal'].fit_transform([[item.diagnostico_principalcat]])[0][0])#diagnostico_principal
+        #dato_en.append(tablas_enc['diagnostico_secundario'].fit_transform([[item.diagnostico_secundariocat]]))#diagnostico_secundario
+        #dato_en.append(tablas_enc['diagnostico_terciario'].fit_transform([[item.diagnostico_terciariocat]]))#diagnostico_terciario
+        #dato_en.append(tablas_enc['cuarto_diagnostico'].fit_transform([[item.cuarto_diagnosticocat]]))#cuarto_diagnostico
+        dato_en.append(float(item.T_a1cat))#T_a1
+        dato_en.append(float(item.T_a2cat))#T_a2
+        prediccion= clf11.predict([dato_en])
+        lists = prediccion.tolist()
+        prediccion2= clf22.predict(lists)
+        lists2 = prediccion2.tolist()
+        respuesta3=0
+        respuesta3={
+              "cups_codigo":tablas_enc2['cups_codigo'].inverse_transform([[prediccion[0][0]]])[0][0],
+              "cups_codigo_probabilidad":max(clf11.predict_proba([dato_en])[0][0]),
+              "descripcion" :tablas_enc2['descripcion'].inverse_transform([[prediccion[0][1]]])[0][0],
+              "descripcion_probabilidad":max(clf11.predict_proba([dato_en])[1][0]),
+              "cantidad":tablas_enc2['cantidad'].inverse_transform([[prediccion[0][2]]])[0][0],
+              "cantidad_probabilidad":max(clf11.predict_proba([dato_en])[2][0]),
+              "prescripcion": tablas_enc2['prescripcion'].inverse_transform([[prediccion[0][3]]])[0][0][0][0],
+              "prescripcion_probabilidad":max(clf11.predict_proba([dato_en])[3][0]),
+              "tipo": tablas_enc2['tipo'].inverse_transform([[prediccion[0][4]]])[0][0],
+              "tipo_probabilidad":max(clf11.predict_proba([dato_en])[4][0]),
+              "codigo_medicamento":tablas_enc2['codigo_medicamento'].inverse_transform([[prediccion2[0][0]]])[0][0],
+              "codigo_medicamento_probabilidad":max(clf22.predict_proba(lists)[0][0]),
+              "descripcion_medicamento":tablas_enc2['descripcion_medicamento'].inverse_transform([[prediccion2[0][1]]])[0][0],
+              "descripcion_medicamento_probabilidad":max(clf22.predict_proba(lists)[1][0]),
+              "cantidad_medicamento":tablas_enc2['cantidad_medicamento'].inverse_transform([[prediccion2[0][2]]])[0][0],
+              "cantidad_medicamento_probabilidad":max(clf22.predict_proba(lists)[2][0]),
+              "prescripcion_medicamento":tablas_enc2['prescripcion_medicamento'].inverse_transform([[prediccion2[0][3]]])[0][0],
+              "prescripcion_medicamento_probabilidad":max(clf22.predict_proba(lists)[3][0]),
+              #"presentacion":tablas_enc['presentacion'].inverse_transform([[prediccion2[0][4]]])[0][0]
+          }
     return respuesta3
